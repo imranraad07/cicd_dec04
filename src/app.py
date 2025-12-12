fb58219add4d99ae3dee4be038f7934ad48d630b
sean <seantheprofessional1@gmail.com>
2025-12-12 15:50:30 -0600

    added more functions

--- src/app.py
+++ src/app.py
@@ -39,5 +39,32 @@ def square_root(a):
         raise Exception("No real answer")
     return math.sqrt(a)
 
+def sine(a):
+    if not is_number(a):
+        raise Exception("Not a number")
+    return math.sin(a)
+
+def cosine(a):
+    if not is_number(a):
+        raise Exception("Not a number")
+    return math.cos(a)
+
+def percentage(a, b):
+    """
+    Return a% of b
+    """
+    if not (is_number(a) and is_number(b)):
+        raise Exception("Not a number")
+    if a < 0:
+        raise Exception("No negative percentages")
+    return (a / 100) * b
+
+def log(base, x):
+    if not (is_number(base) and is_number(x)):
+        raise Exception("Not a number")    
+    if x < 0 or base <= 0:
+        raise Exception("Base and x cannot be negative")
+    return math.log(x, base)
+
 if __name__ == "__main__":
     print(add("5.1", 1.9))
