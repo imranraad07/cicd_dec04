6032c9542a34c48261f143b814e4603cfc981622
Robert Boerwinkle <rfboerwinkle@gmail.com>
2025-12-12 18:05:06 -0600

    implement basic functions

--- src/app.py
+++ src/app.py
@@ -1,5 +1,28 @@
+import math
+
 def add (a, b):
     return a+b
 
 def sub (a, b):
     return a-b
+
+def log (a, base):
+    return math.log(a, base)
+
+def square (a):
+    return a**2
+
+def sin (a):
+    return math.sin(a)
+
+def cos (a):
+    return math.cos(a)
+
+def sqrt (a):
+    try:
+        return math.sqrt(a)
+    except ValueError:
+        return float("nan")
+
+def percentage (a):
+    return a/100
